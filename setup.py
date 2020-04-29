23f2e04b96046e39121a598d0484710f98482875
srikanthiremath <srikanthiremath750.com>
2020-04-29 16:04:39 +0530

    fourth

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='print-msg',
+    name='discover-dollar-name-match',
     version='0.0.1',
     description='My private package from private github repo',
-    url='https://github.com/discoverdollartech/print-msg',
+    url='https://github.com/discoverdollartech/discover-dollar-name-match',
     author='srikant hiremath',
     author_email='srikant.hiremath@discoverdollar.com',
     license='MIT',
