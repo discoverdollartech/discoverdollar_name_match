82ae8078b70cf26ddfc096f6602b16b677ae4adf
srikanthiremath <srikanthiremath750.com>
2020-04-29 19:36:08 +0530

    'five'

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='discover_dollar_name_match',
+    name='ddollar_name_match',
     version='0.0.1',
     description='My private package from private github repo',
-    url='https://github.com/discoverdollartech/discover_dollar_name_match',
+    url='https://github.com/discoverdollartech/ddollar_name_match',
     author='srikant hiremath',
     author_email='srikant.hiremath@discoverdollar.com',
     license='MIT',
-    packages=['discover_dollar_name_match'],
+    packages=['ddollar_name_match'],
     zip_safe=False
 )
