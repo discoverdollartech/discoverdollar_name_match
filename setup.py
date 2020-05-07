b0689136e24ec3b3024ec0e6df4927c0577cb1f8
srikanthiremathDD <56291294+srikanthiremathDD@users.noreply.github.com>
2020-05-07 17:22:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     author_email='srikant.hiremath@discoverdollar.com',
     license='MIT',
     packages=['ddollar_name_match'],
-    install_requires=['pandas>=0.24.2','fuzzywuzzy>=0.18.0','cleanco','numpy>=1.16.2']
+    install_requires=['pandas>=0.24.2','fuzzywuzzy>=0.18.0','cleanco','numpy>=1.16.2'],
     zip_safe=False
 )
