bb45c2dad8b8a0995a914994d1b40618495d00b9
srikanthiremathDD <56291294+srikanthiremathDD@users.noreply.github.com>
2020-05-07 17:56:46 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='ddollar_name_match',
+    name='discoverdollar_name_match',
     version='0.0.1',
     description='My private package from private github repo',
-    url='https://github.com/discoverdollartech/ddollar_name_match',
+    url='https://github.com/discoverdollartech/discoverdollar_name_match',
     author='srikant hiremath',
     author_email='srikant.hiremath@discoverdollar.com',
     license='MIT',
-    packages=['ddollar_name_match'],
+    packages=['discoverdollar_name_match'],
     install_requires=['pandas>=0.24.2','fuzzywuzzy>=0.18.0','cleanco','numpy>=1.16.2'],
     zip_safe=False
 )
